c07644cae8ed9cb8b1516a6958ec64fcc014d614
Matthew Mullins <4397399@myuwc.ac.za>
2026-03-16 14:18:05 +0200

    attempting heapify code and declaration of tryHeapsort

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,30 @@
 
 void main() {
 
+     class tryHeapsort {
+        static int heapSize;
+        static void swap(String[] A, int i, int j) {
+            String temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+     }
+
+     void heapify(String[] A, int i) {
+
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int largest = i;
+
+        if (left < heapSize && A[left].compareTo(A[largest]) > 0)
+            largest = left;
+
+        if (right < heapSize && A[right].compareTo(A[largest]) > 0)
+            largest = right;
+
+        if (largest != i) {
+            swap(A,i,largest);
+            heapify(A,largest);
+        }
+    }
 }
